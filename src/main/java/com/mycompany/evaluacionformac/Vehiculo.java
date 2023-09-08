faebb02a92bcf44f24c072070b4eb13931dac543
vina <vina@VIAWSE02LC0426.duoc.acad>
2023-09-07 22:10:53 -0300

    evaluacion de java Daniel Gajardo

---
+++ src/main/java/com/mycompany/evaluacionformac/Vehiculo.java
@@ -0,0 +1,99 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.evaluacionformac;
+
+/**
+ *
+ * @author vina
+ */
+public class Vehiculo {
+
+    private String marca;
+    private String modelo;
+    private String color;
+    private String duenio;
+    private int canPuertas;
+    private int anio;
+
+    public Vehiculo() {
+    }
+
+    public Vehiculo(String marca, String modelo, String color, String duenio, int canPuertas, int anio) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.color = color;
+        this.duenio = duenio;
+        this.canPuertas = canPuertas;
+        this.anio = anio;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getDuenio() {
+        return duenio;
+    }
+
+    public void setDuenio(String duenio) {
+        this.duenio = duenio;
+    }
+
+    public int getCanPuertas() {
+        return canPuertas;
+    }
+
+    public void setCanPuertas(int canPuertas) {
+        this.canPuertas = canPuertas;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public static void mostrar() {
+        System.out.println("MARCA: " + this.marca);
+        System.out.println("MODELO: " + this.modelo);
+        System.out.println("COLOR: " + this.color);
+        System.out.println("DUEÑO" + this.duenio);
+        System.out.println("CANTIDAD DE PUERTAS: " + canPuertas);
+        System.out.println("AÑO: " + this.anio);
+        
+    
+        
+     
+    public static void registrar(){
+        else if int 
+            km;
+        
+        int km;
+            if km < 500000
+        
+    }
+}
